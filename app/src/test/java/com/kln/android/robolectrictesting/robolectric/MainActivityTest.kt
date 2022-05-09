92c14c4621c8f6de14ad8f7fa2866dbb1f3c1f40
Hasini <hasinisama99@gmail.com>
2022-05-09 21:37:30 +0530

    Fragment test configured

--- app/src/test/java/com/kln/android/robolectrictesting/robolectric/MainActivityTest.kt
+++ app/src/test/java/com/kln/android/robolectrictesting/robolectric/MainActivityTest.kt
@@ -11,6 +11,7 @@ import org.junit.Test
 import org.junit.runner.RunWith
 import org.robolectric.Robolectric
 import org.robolectric.RobolectricTestRunner
+import org.robolectric.annotation.Config
 
 @RunWith(RobolectricTestRunner::class)
 class MainActivityTest {
